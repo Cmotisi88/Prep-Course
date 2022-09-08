c81b731637f06fc70f4a4552721dcfc5e1730670
cristian <argentino.mza@gmail.comgit config --global user.name cristiangit commit -m FIRST>
2022-09-08 00:09:19 -0300

    homework done js V

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -86,7 +86,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
-  //Ej: "Juan, 22 años"
+  //Ej: "Juan, 22 años" 
   Persona.prototype.datos = function(nombre,edad){
     return this.nombre + ", " + this.edad + " años"
   }
